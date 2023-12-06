5db77dd89f079390aabb7f470b00b73be120c92c
K H Arjun <134183309+ArjunKH2004@users.noreply.github.com>
2023-12-06 12:08:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,42 @@
 var customName = document.getElementById('customname');
-var  randomize = document.querySelector('.randomize');
+var randomize = document.querySelector('.randomize');
 var story = document.querySelector('.story');
 
-var storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-var insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-var insertY = ['the soup kitchen','Disneyland','the White House'];
-var insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+var storyText = 'On a chilly winter day, with the temperature at a freezing 20 degrees, :insertx: decided to go for a walk. When they arrived at :inserty:, they couldn\'t believe their eyes. After staring in amazement for a few moments, :insertz:. Bob saw the whole thing, but was not surprised — :insertx: is known for wearing three layers of heavy winter clothing, and it was an unusually cold day.';
+var insertX = ['Frosty the Snowman','Penguin Pete','Eskimo Ed'];
+var insertY = ['an ice cream parlor','the ski resort','Santa\'s workshop'];
+var insertZ = ['froze solid','shivered uncontrollably','transformed into an icicle and melted away'];
 
 function randomValFrmArr(array) {
-	// console.log('Function Called');
-	return array[Math.floor(Math.random()*array.length)];
+  return array[Math.floor(Math.random()*array.length)];
 }
 
 randomize.addEventListener('click', result);
 
 function result() {
 
-	var newStory = storyText;
-	var itemX = randomValFrmArr(insertX);
-	var itemY = randomValFrmArr(insertY);
-	var itemZ = randomValFrmArr(insertZ);
+  var newStory = storyText;
+  var itemX = randomValFrmArr(insertX);
+  var itemY = randomValFrmArr(insertY);
+  var itemZ = randomValFrmArr(insertZ);
 
-	newStory = newStory.replace(':insertx:',itemX);
-	newStory = newStory.replace(':insertx:',itemX);
-	newStory = newStory.replace(':inserty:',itemY);
-	newStory = newStory.replace(':insertz:',itemZ);
-	// console.log(newStory);
+  newStory = newStory.replace(':insertx:',itemX);
+  newStory = newStory.replace(':inserty:',itemY);
+  newStory = newStory.replace(':insertz:',itemZ);
 
   if(customName.value != '') {
     var name = customName.value;
-    newStory = newStory.replace('Bob',name);
-
+    newStory = newStory.replace(/Bob/g, name);
   }
+
 
   if(document.getElementById("uk").checked) {
     var weight = Math.round(300*0.071429) + ' stone';
-    var temperature =  Math.round((94-32)*5/9)+ ' centigrade';
-    newStory = newStory.replace('94 farenheit',temperature);
-    newStory = newStory.replace('300 pounds',weight);
-    
+    var temperature =  Math.round((20-32)*5/9) + ' centigrade'; 
+    newStory = newStory.replace('20 degrees', temperature);
+    newStory = newStory.replace('300 pounds', weight);
   }
 
   story.textContent = newStory;
   story.style.visibility = 'visible';
 }
-
-
-
